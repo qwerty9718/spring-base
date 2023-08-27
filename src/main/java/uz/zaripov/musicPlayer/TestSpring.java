4f00c1d04c9b0c668e759d788c2601a4f460eef6
Zaripov Albert <qwerty_97_18@mail.ru>
2023-08-27 11:25:12 +0500

    Bean Scope , Destroy-Method , Init-Method

--- src/main/java/uz/zaripov/musicPlayer/TestSpring.java
+++ src/main/java/uz/zaripov/musicPlayer/TestSpring.java
@@ -1,15 +1,14 @@
 package uz.zaripov.musicPlayer;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
 
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                "applicationContext.xml"
-        );
 
-        MusicPlayer player = context.getBean("musicPlayer",MusicPlayer.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+
         Computer computer = context.getBean("computer", Computer.class);
         computer.startMusicPlayer();
 
