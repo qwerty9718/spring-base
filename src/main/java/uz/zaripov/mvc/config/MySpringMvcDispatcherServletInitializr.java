6b5419f7262fbcedf455e10fa88db9ffe8c5ea89
Zaripov Albert <qwerty_97_18@mail.ru>
2023-08-27 12:25:12 +0500

    Mvc with java code

---
+++ src/main/java/uz/zaripov/mvc/config/MySpringMvcDispatcherServletInitializr.java
@@ -0,0 +1,24 @@
+package uz.zaripov.mvc.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializr extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+
+}
